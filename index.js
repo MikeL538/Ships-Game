e738c9aa530e02939d490cba9d6d1ff1b7b631aa
MikeL538 <126614886+MikeL538@users.noreply.github.com>
2023-08-18 01:16:23 +0200

    Update New Game
    Added option to start new game after victory

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ function generateAllShips() {
   /////////////////////////////
   oneShips = [];
   for (let i = 0; i < oneShipCount; i++) {
-    const ship = [];
-    let startCell;
-    let direction;
+    let ship = [];
+    let startCell = null;
+    let direction = null;
 
     // Losowy wybór kierunku (pionowy lub poziomy)
     if (Math.random() < 0.5) {
@@ -109,9 +109,9 @@ function generateAllShips() {
   /////////////////////////////
   twoShips = [];
   for (let i = 0; i < twoShipsCount; i++) {
-    const ship = [];
-    let startCell;
-    let direction;
+    ship = [];
+    startCell = null;
+    direction = null;
 
     // Losowy wybór kierunku (pionowy lub poziomy)
     if (Math.random() < 0.5) {
@@ -177,9 +177,9 @@ function generateAllShips() {
 
   threeShips = [];
   for (let i = 0; i < threeShipsCount; i++) {
-    const ship = [];
-    let startCell;
-    let direction;
+    ship = [];
+    startCell = null;
+    direction = null;
 
     // Losowy wybór kierunku (pionowy lub poziomy)
     if (Math.random() < 0.5) {
@@ -246,8 +246,8 @@ function generateAllShips() {
   fourShips = [];
   for (let i = 0; i < fourShipsCount; i++) {
     const ship = [];
-    let startCell;
-    let direction;
+    startCell = null;
+    direction = null;
 
     // Losowy wybór kierunku (pionowy lub poziomy)
     if (Math.random() < 0.5) {
@@ -312,7 +312,6 @@ function generateAllShips() {
 function handleShot(cellIndex) {
   shots++;
   const cell = cells[cellIndex];
-  cell.removeEventListener("click", handleShot);
 
   // Sprawdzanie trafienia
   let hit = false;
@@ -350,12 +349,12 @@ function handleShot(cellIndex) {
         msgSunken.textContent = "You sunk a ship!";
         setTimeout(() => {
           msgSunken.textContent = "";
-        }, 1000);
+        }, 700);
 
         table.style.pointerEvents = "none";
         setTimeout(() => {
           table.style.pointerEvents = "all";
-        }, 600);
+        }, 500);
       } else {
         msgMain.textContent = "Hit!";
       }
@@ -370,29 +369,35 @@ function handleShot(cellIndex) {
   }
 
   // Aktualizacja tabeli pozostałych statków
-  msgAdditional.innerHTML = `Shots: ${shots} <br> 
-  Sunken: ${hits}/20 <br>  <br> 
+  msgAdditional.innerHTML = `Shots: ${shots}<br /> 
+  Sunken: ${hits}/20<br /><br /> 
 
   Remaining Ships: <br>
-  One square ships: ${oneShipsHit}   <br />
+  One square ships: ${oneShipsHit}<br />
   Two squares ships: ${Math.round(twoShipsHit)}<br />
-  Three squares ships: ${Math.round(threeShipsHit)} <br />
+  Three squares ships: ${Math.round(threeShipsHit)}<br />
   Four squares ship: ${Math.round(fourShipsHit)}`;
 
-  if (hits === 20) {
+  if (hits === 1) {
     // Wszystkie statki zatopione, koniec gry
     table.style.pointerEvents = "none";
-    msgMain.innerHTML = `Victory! <br> 
-    Congratulations! <br> <br>
-    Amount of shots: ${shots} <br>`;
+    msgMain.innerHTML = `Victory!<br /> 
+    Congratulations!<br /><br />
+    Amount of shots: ${shots}<br />`;
 
     newGameButtonVictory.classList.add("show");
     msgMain.classList.add("message-container--victory");
     newGameButtonVictory.classList.add("message-container--victory");
     window.scrollTo(0, 0);
-    window.alert("Victory! Congratulations!");
     cells.forEach((cell) => cell.removeEventListener("click", handleShot));
   }
+}
+
+function handleShotStart() {
+  cells.forEach((cell, index) => {
+    cell.classList.remove("hit", "miss", "ship");
+    cell.addEventListener("click", () => handleShot(index));
+  });
 }
 
 // Inicjalizacja gry
@@ -415,24 +420,19 @@ function initGame() {
   msgMain.classList.remove("message-container--victory");
   newGameButtonVictory.classList.remove("show");
 
-  // // Usunięcie klas "hit", "miss" oraz "ship" z komórek
-  // cells.forEach((cell) => {
-  //   cell.classList.remove("hit", "miss", "ship");
-  // });
-
-  cells.forEach((cell, index) => {
-    cell.addEventListener("click", () => handleShot(index));
+  cells.forEach((cell) => {
+    cell.classList.remove("hit", "miss", "ship");
   });
 
   generateAllShips();
 
-  msgAdditional.innerHTML = `Shots: ${shots} <br> 
-  Sunken: ${hits}/20 <br> <br> 
+  msgAdditional.innerHTML = `Shots: ${shots}<br /> 
+  Sunken: ${hits}/20<br /><br /> 
 
-  Remaining Ships: <br>
-  One square ships: ${oneShipsHit}   <br />
+  Remaining Ships:<br />
+  One square ships: ${oneShipsHit}<br />
   Two squares ships: ${twoShipsHit}<br />
-  Three squares ships: ${threeShipsHit} <br />
+  Three squares ships: ${threeShipsHit}<br />
   Four squares ship: ${fourShipsHit}`;
 
   table.style.pointerEvents = "all";
@@ -446,366 +446,10 @@ newGameButton.addEventListener("click", () => {
       mainMenu.style.display = "none";
     }, 4000);
   mainMenu.classList.toggle("main-menu--hide");
+  handleShotStart();
   initGame();
 });
 
 newGameButtonVictory.addEventListener("click", () => {
   initGame();
 });
-
-// function newTable() {
-//   table.innerHTML = `
-//   <tr id="alphabet">
-//     <td>&nbsp;</td>
-//     <td>A</td>
-//     <td>B</td>
-//     <td>C</td>
-//     <td>D</td>
-//     <td>E</td>
-//     <td>F</td>
-//     <td>G</td>
-//     <td>H</td>
-//     <td>I</td>
-//     <td>J</td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">1</td>
-//     <td>
-//       <div id="1a"></div>
-//     </td>
-//     <td>
-//       <div id="1b"></div>
-//     </td>
-//     <td>
-//       <div id="1c"></div>
-//     </td>
-//     <td>
-//       <div id="1d"></div>
-//     </td>
-//     <td>
-//       <div id="1e"></div>
-//     </td>
-//     <td>
-//       <div id="1f"></div>
-//     </td>
-//     <td>
-//       <div id="1g"></div>
-//     </td>
-//     <td>
-//       <div id="1h"></div>
-//     </td>
-//     <td>
-//       <div id="1i"></div>
-//     </td>
-//     <td>
-//       <div id="1j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">2</td>
-//     <td>
-//       <div id="2a"></div>
-//     </td>
-//     <td>
-//       <div id="2b"></div>
-//     </td>
-//     <td>
-//       <div id="2c"></div>
-//     </td>
-//     <td>
-//       <div id="2d"></div>
-//     </td>
-//     <td>
-//       <div id="2e"></div>
-//     </td>
-//     <td>
-//       <div id="2f"></div>
-//     </td>
-//     <td>
-//       <div id="2g"></div>
-//     </td>
-//     <td>
-//       <div id="2h"></div>
-//     </td>
-//     <td>
-//       <div id="2i"></div>
-//     </td>
-//     <td>
-//       <div id="2j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">3</td>
-//     <td>
-//       <div id="3a"></div>
-//     </td>
-//     <td>
-//       <div id="3b"></div>
-//     </td>
-//     <td>
-//       <div id="3c"></div>
-//     </td>
-//     <td>
-//       <div id="3d"></div>
-//     </td>
-//     <td>
-//       <div id="3e"></div>
-//     </td>
-//     <td>
-//       <div id="3f"></div>
-//     </td>
-//     <td>
-//       <div id="3g"></div>
-//     </td>
-//     <td>
-//       <div id="3h"></div>
-//     </td>
-//     <td>
-//       <div id="3i"></div>
-//     </td>
-//     <td>
-//       <div id="3j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">4</td>
-//     <td>
-//       <div id="4a"></div>
-//     </td>
-//     <td>
-//       <div id="4b"></div>
-//     </td>
-//     <td>
-//       <div id="4c"></div>
-//     </td>
-//     <td>
-//       <div id="4d"></div>
-//     </td>
-//     <td>
-//       <div id="4e"></div>
-//     </td>
-//     <td>
-//       <div id="4f"></div>
-//     </td>
-//     <td>
-//       <div id="4g"></div>
-//     </td>
-//     <td>
-//       <div id="4h"></div>
-//     </td>
-//     <td>
-//       <div id="4i"></div>
-//     </td>
-//     <td>
-//       <div id="4j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">5</td>
-//     <td>
-//       <div id="5a"></div>
-//     </td>
-//     <td>
-//       <div id="5b"></div>
-//     </td>
-//     <td>
-//       <div id="5c"></div>
-//     </td>
-//     <td>
-//       <div id="5d"></div>
-//     </td>
-//     <td>
-//       <div id="5e"></div>
-//     </td>
-//     <td>
-//       <div id="5f"></div>
-//     </td>
-//     <td>
-//       <div id="5g"></div>
-//     </td>
-//     <td>
-//       <div id="5h"></div>
-//     </td>
-//     <td>
-//       <div id="5i"></div>
-//     </td>
-//     <td>
-//       <div id="5j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">6</td>
-//     <td>
-//       <div id="6a"></div>
-//     </td>
-//     <td>
-//       <div id="6b"></div>
-//     </td>
-//     <td>
-//       <div id="6c"></div>
-//     </td>
-//     <td>
-//       <div id="6d"></div>
-//     </td>
-//     <td>
-//       <div id="6e"></div>
-//     </td>
-//     <td>
-//       <div id="6f"></div>
-//     </td>
-//     <td>
-//       <div id="6g"></div>
-//     </td>
-//     <td>
-//       <div id="6h"></div>
-//     </td>
-//     <td>
-//       <div id="6i"></div>
-//     </td>
-//     <td>
-//       <div id="6j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">7</td>
-//     <td>
-//       <div id="7a"></div>
-//     </td>
-//     <td>
-//       <div id="7b"></div>
-//     </td>
-//     <td>
-//       <div id="7c"></div>
-//     </td>
-//     <td>
-//       <div id="7d"></div>
-//     </td>
-//     <td>
-//       <div id="7e"></div>
-//     </td>
-//     <td>
-//       <div id="7f"></div>
-//     </td>
-//     <td>
-//       <div id="7g"></div>
-//     </td>
-//     <td>
-//       <div id="7h"></div>
-//     </td>
-//     <td>
-//       <div id="7i"></div>
-//     </td>
-//     <td>
-//       <div id="7j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">8</td>
-//     <td>
-//       <div id="8a"></div>
-//     </td>
-//     <td>
-//       <div id="8b"></div>
-//     </td>
-//     <td>
-//       <div id="8c"></div>
-//     </td>
-//     <td>
-//       <div id="8d"></div>
-//     </td>
-//     <td>
-//       <div id="8e"></div>
-//     </td>
-//     <td>
-//       <div id="8f"></div>
-//     </td>
-//     <td>
-//       <div id="8g"></div>
-//     </td>
-//     <td>
-//       <div id="8h"></div>
-//     </td>
-//     <td>
-//       <div id="8i"></div>
-//     </td>
-//     <td>
-//       <div id="8j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">9</td>
-//     <td>
-//       <div id="9a"></div>
-//     </td>
-//     <td>
-//       <div id="9b"></div>
-//     </td>
-//     <td>
-//       <div id="9c"></div>
-//     </td>
-//     <td>
-//       <div id="9d"></div>
-//     </td>
-//     <td>
-//       <div id="9e"></div>
-//     </td>
-//     <td>
-//       <div id="9f"></div>
-//     </td>
-//     <td>
-//       <div id="9g"></div>
-//     </td>
-//     <td>
-//       <div id="9h"></div>
-//     </td>
-//     <td>
-//       <div id="9i"></div>
-//     </td>
-//     <td>
-//       <div id="9j"></div>
-//     </td>
-//   </tr>
-
-//   <tr class="numbers">
-//     <td class="numbers-color">0</td>
-//     <td>
-//       <div id="0a"></div>
-//     </td>
-//     <td>
-//       <div id="0b"></div>
-//     </td>
-//     <td>
-//       <div id="0c"></div>
-//     </td>
-//     <td>
-//       <div id="0d"></div>
-//     </td>
-//     <td>
-//       <div id="0e"></div>
-//     </td>
-//     <td>
-//       <div id="0f"></div>
-//     </td>
-//     <td>
-//       <div id="0g"></div>
-//     </td>
-//     <td>
-//       <div id="0h"></div>
-//     </td>
-//     <td>
-//       <div id="0i"></div>
-//     </td>
-//     <td>
-//       <div id="0j"></div>
-//     </td>
-//   </tr>`;
-// }
